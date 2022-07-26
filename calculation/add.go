94af23bb39bdd435ce23db3ef9145e80c3937bc7
Gempur Bayu Aji <aji123zz@gmail.com>
2022-07-26 12:21:33 +0700

    create example library

---
+++ calculation/add.go
@@ -0,0 +1,12 @@
+package calculation
+
+//nama func kapital artinya public bisa diakses cross package
+func Add(number int, numberTwo int) int {
+	return number + numberTwo
+	//return min(number, numberTwo)
+}
+
+//contoh private func yang tidak bisa diakses di package lain
+func min(number int, numberTwo int) int {
+	return number - numberTwo
+}
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"pertama/calculation"
+)
 
+//untuk menjalankan func diluar package main/beda package menggunakan import
+//"namamodule/package" seperti contoh diatas pertama/calculation
 func main() {
 	fmt.Println("Halo Golang!")
 
 	sentence := Testing()
 
 	fmt.Println(sentence)
+
+	result := calculation.Add(2, 3)
+	fmt.Println(result)
 }
